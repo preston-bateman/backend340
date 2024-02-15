89ea9ba3baa5b28dc10b2e50777df83dc7f7cf21
Preston Bateman <prestonbbateman@gmail.com>
2024-02-15 16:43:36 -0600

    push

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
+const regValidate = require("../utilities/account-validation")
 
 //Login Route
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
@@ -11,7 +12,12 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/registration", utilities.handleErrors(accountController.buildRegistration))
 
 //Registration Post
-router.post('/registration', utilities.handleErrors(accountController.registerAccount))
+router.post(
+    '/registration',
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+     utilities.handleErrors(accountController.registerAccount)
+     )
 
 //Export Router
 module.exports = router;
