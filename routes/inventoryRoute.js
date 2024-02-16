340f3a9013a24c587dba20b3e83cf6221dbf3350
Preston Bateman <prestonbbateman@gmail.com>
2024-02-16 00:00:40 -0600

    week 4 inv rules validation

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const utilities = require("../utilities")
+const regValidate = require("../utilities/inventory-validation")
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
@@ -15,5 +16,24 @@ router.get("/", utilities.handleErrors(invController.buildManagement))
 
 // Route to build add classification view
 router.get("/add-classification", utilities.handleErrors(invController.buildAddClassification))
+
+//Process the add classification
+router.post(
+    "/add-classification",
+    regValidate.classificationRules(),
+    regValidate.checkClassData,
+    utilities.handleErrors(invController.addClassification)
+)
+
+// Route to build add inventory view
+router.get("/add-inventory", utilities.handleErrors(invController.buildAddInventory))
+
+//Process the add inventory form
+router.post(
+    "/add-inventory",
+    regValidate.inventoryRules(),
+    regValidate.checkInvData,
+    utilities.handleErrors(invController.addInventory)
+)
 
 module.exports = router;
--- utilities/inventory-validation.js
+++ utilities/inventory-validation.js
@@ -15,3 +15,67 @@ validate.classificationRules = () =>{
             .withMessage("Please Enter in a valid classification name")
     ]
 }
+
+/*  **********************************
+ *  Check Classification Data and return errors
+ * ********************************* */
+validate.checkClassData = async (req, res, next) => {
+    const { classification_name } = req.body
+    let errors = []
+    errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        let nav = await utilities.nav()
+        res.render("inventory/add-classification", {
+            errors,
+            title: "Add Classification",
+            nav,
+            classification_name
+        })
+        return
+    }
+    next()
+}
+
+/*  **********************************
+ *  Inventory Data Validation Rules
+ * ********************************* */
+validate.inventoryRules = () => {
+    return [
+        body("inv_make")
+            .trim()
+            .isLength({ min: 3 })
+            .withMessage("Please enter a valid vehicle make."),
+        body("inv_model")
+            .trim()
+            .isLength({ min: 3 })
+            .withMessage("Please enter a valid vehicle model."),
+        body("inv_year")
+            .trim()
+            .isLength({ max: 4, min: 4 })
+            .withMessage("Please enter a valid vehicle year."),
+        body("inv_description")
+            .trim()
+            .isLength({ max: 150, min: 1 })
+            .withMessage("Please enter a valid vehicle description."),
+        body("inv_image")
+            .trim()
+            .isLength({ min: 3 })
+            .withMessage("Please enter a valid vehicle image."),
+        body("inv_thumbnail")
+            .trim()
+            .isLength({ min: 3 })
+            .withMessage("Please enter a valid vehicle thumbnail."),
+        body("inv_price")
+            .trim()
+            .isLength({ min: 1 })
+            .withMessage("Please enter a valid vehicle price."),
+        body("inv_miles")
+            .trim()
+            .isLength({ min: 1 })
+            .withMessage("Please enter a valid vehicle miles."),
+        body("inv_color")
+            .trim()
+            .isLength({ min: 1 })
+            .withMessage("Please enter a valid vehicle color."),
+    ]
+}
